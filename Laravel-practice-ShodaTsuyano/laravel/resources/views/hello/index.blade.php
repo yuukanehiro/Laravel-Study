c647517ec4b5b35dab7148ce196055741159772a
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-04 23:35:29 +0900

    Start Laravel-practice-ShodaTsuyano

---
+++ Laravel-practice-ShodaTsuyano/laravel/resources/views/hello/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>Index</title>
+</head>
+<body>
+    <h1>Hello/Index</h1>
+    <p>{{$msg}}</p>
+</body>
+</html>
---
+++ Laravel-practice-ShodaTsuyano/laravel/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/hello', 'HelloController@index')->name('hello');
+Route::get('/hello/{id}', 'HelloController@index')->where('id', '[0-9]+');
+Route::get('/hello/other', 'HelloController@other');
