03649a579aa14922dc573b45bc506f09979e55fb
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-05 18:29:04 +0900

    Responseと出力コンテンツ

--- Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
+++ Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\Person;
 
 class HelloController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, Response $response)
     {
         $msg    = 'please input text:';
         $keys   = [];
         $values = [];
         if ($request->isMethod('post')) {
             $form   = $request->all();
-            $keys   = array_keys($form);
-            $values = array_values($form);
+            $result = '<html><body>';
+            foreach($form as $key => $value)
+            {
+                $result .= $key . ': ' . $value . "<br>";
+            }
+            $result .= '</body></html>';
+            $response->setContent($result);
+            return $response;
         }
         $data = [
             'msg'    => $msg,
