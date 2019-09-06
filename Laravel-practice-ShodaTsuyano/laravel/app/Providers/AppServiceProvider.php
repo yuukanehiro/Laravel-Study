9c970bf975f2d7e2584b43f35f77feb13ce9d7af
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-06 20:27:15 +0900

    P90-92 結合時のイベント処理

--- Laravel-practice-ShodaTsuyano/laravel/app/Providers/AppServiceProvider.php
+++ Laravel-practice-ShodaTsuyano/laravel/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\MyClasses\MyService;
+use App\MyClasses\PowerMyService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        app()->resolving(function($obj, $app){
+
+            if(is_object($obj))
+            {
+                echo get_class($obj) . '<br>';
+            }else{
+                echo $obj . '<br>';
+            }
+        });
+        app()->resolving(PowerMyService::class, function($obj, $app){
+            $newdata = ['ハンバーグ', 'カレーライス', '唐揚げ', '餃子'];
+            $obj->setData($newdata);
+            $obj->setId(rand(0, count($newdata)));
+        });
+
         app()->bind('App\MyClasses\MyServiceInterface',
             // 'App\MyClasses\MyService');
             'App\MyClasses\PowerMyService');
