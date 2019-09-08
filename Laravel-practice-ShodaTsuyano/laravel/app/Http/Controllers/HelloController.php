eeb181915922043a117045c54aa7615b52a0162f
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-08 22:43:27 +0900

    P119-120 最初と最後のレコードを取得する

--- Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
+++ Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
@@ -9,14 +9,10 @@ class HelloController extends Controller
 {
     public function index($name = "hoge")
     {
-        if ($name != '') {
-            $msg = 'get name like ' . $name . '"';
-            $result = DB::table('people')
-                ->whereRaw('name like ?', ['%'.$name.'%'])->get();
-        } else {
-            $msg = 'get people records';
-            $result = DB::table('people')->get();
-        }
+        $msg = "get people records.";
+        $first = DB::table('people')->first();
+        $last  = DB::table('people')->orderBy('id', 'desc')->first();
+        $result = [$first, $last];
 
         $data = [
             'msg'  => 'Database access',
