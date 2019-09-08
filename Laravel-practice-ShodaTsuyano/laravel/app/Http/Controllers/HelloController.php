890f781be21350a75b28152628e786e233d2f44f
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-08 20:27:14 +0900

    P117-118 あいまい検索はどうする？

--- Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
+++ Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
@@ -1,19 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index($id = -1)
+    public function index($name = "hoge")
     {
-        if($id >= 0)
-        {
-            $msg = "get ID <= " . $id;
-            $result = DB::table('people')->where('id', '<=', $id)->get();
-        }
-        else{
+        if ($name != '') {
+            $msg = 'get name like ' . $name . '"';
+            $result = DB::table('people')
+                ->where('name', 'like', '%' . $name . '%')->get();
+        } else {
             $msg = 'get people records';
             $result = DB::table('people')->get();
         }
