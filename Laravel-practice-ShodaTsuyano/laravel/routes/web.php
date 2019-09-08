9b9e7be0f83d7a754bab69f738dc07b42b158bbf
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-08 23:33:10 +0900

    P120-121 指定IDのレコード取得(find)

--- Laravel-practice-ShodaTsuyano/laravel/routes/web.php
+++ Laravel-practice-ShodaTsuyano/laravel/routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Middleware\HelloMiddleware;
 //     Route::get('/hello', 'HelloController@index');
 //     Route::get('/hello/other', 'HelloController@other');
 // });
-Route::get('/hello/{name}', 'HelloController@index');
+Route::get('/hello/{id}', 'HelloController@index');
 Route::get('/hello/', 'HelloController@index');
 
 Route::namespace('Sample')->group(function(){
