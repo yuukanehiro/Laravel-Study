dc94df123c4af23ee10526ac77a81429a00b8341
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-09-11 02:09:27 +0900

    P148-150 diffによる差分の取得

--- Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
+++ Laravel-practice-ShodaTsuyano/laravel/app/Http/Controllers/HelloController.php
@@ -12,14 +12,20 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $msg = 'show people record.';
-        $result = Person::get()->reject(function($person)
+        $result = Person::get()->filter(function($person)
         {
-            return $person->age < 20;
+            return $person->age < 70;
         });
+        $result2 = Person::get()->filter(function($person)
+        {
+            return $person->age < 10;
+        });
+        // 70歳以下のレコード群から、10歳以下のレコードを取り除いたもの
+        $result3 = $result->diff($result2);
 
         $data = [
             'msg'  => $msg,
-            'data' => $result
+            'data' => $result3
         ];
         return view('hello.index', $data);
     }
